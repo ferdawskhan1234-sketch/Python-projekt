988c0a0fb1b490d8aab08b0ba17973176f2d3e45
Ferdaws khan <Ferdawaskhan1234@gmail.com>
2026-08-21 22:10:16 +0200

    Över på listor

--- python. loop.py
+++ python. loop.py
@@ -1,1 +1,19 @@
+list=[1, 2, 3, 44, 4, 2, 5, 6,]
+print(list)
+
+list.pop(2)
+print(list)
+list.append(10)
+print(list)
+list.insert(1, 99)
+print(list)
+print(list.count(2))
+list.insert(3, 3)
+print(list)
+list.append(10)
+print(list)
+list.pop(3)
+print(list)
+list.insert(3, 99)
+print(list)
 
