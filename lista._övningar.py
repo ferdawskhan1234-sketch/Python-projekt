24b44fa35cc399e275d026a42962e878259863e1
Ferdaws khan <Ferdawaskhan1234@gmail.com>
2026-08-21 23:08:14 +0200

    Träniar på listor

---
+++ lista._övningar.py
@@ -0,0 +1,24 @@
+lista2 = [22, 1, 4, 66, 2, 2, 2, 77, 10, 5, 12]
+print(lista2)
+lista2.remove(4)
+print(lista2)
+lista2.append(30)
+print(lista2)
+lista2.insert(1, 7)
+print(lista2)
+lista2.sort()
+print(lista2)
+print(lista2.count(2))
+print(lista2.count(5))
+print(len(lista2))
+lista2.remove(66)
+print(lista2)
+lista2.sort()
+print(lista2)
+lista2.reverse()
+print(lista2)
+lista3 =["banan", "äpple", "apelsin", "päron"]
+print(lista3)
+print(len(lista3))
+
+
