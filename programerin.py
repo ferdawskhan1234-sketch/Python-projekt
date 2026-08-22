97582abbab44c30f06f72ce4b07d4b8cdce5a838
Ferdaws khan <Ferdawaskhan1234@gmail.com>
2026-08-22 15:32:22 +0200

    spara ändring

--- programerin.py
+++ programerin.py
@@ -1,11 +1,25 @@
-lista=[1, 2, 3, 4, 5, 6]
-print(lista)
+my_lista = ["Faza", "Qesmat", "Moska", "Visal", "ferdaws", "khan"]
+print(my_lista)
+my_lista.append("khan11")
+print(my_lista)
 
-lista.pop(3)
-print(lista)
-lista.insert(1, 99)
-print(lista)
-lista.append("ferdaws")
-print(lista)
+you_lista = [2, 1, 3, 4, 6, 5, 7]
+you_lista.pop(3)
+print(you_lista)
 
+lista3 = [1, 2, 3, 4, 5,]
+lista3.insert(2, 100)
+print(lista3)
 
+lista4 = [2, 44, 5, 44, 33,33,33,33,33, 3, 3, 4, 4, 4]
+print(lista4.count(33))
+
+lista5 = [2, 44, 5, 44, 33,33,33,33,33, 3, 3, 4, 4, 4]
+lista5.sort()
+print(lista5)
+
+lista5.sort(reverse=True)
+print(lista5)
+
+din_lista = ["Faza", "Qesmat", "Moska", "Visal", "ferdaws", "khan"]
+print(len(din_lista))
