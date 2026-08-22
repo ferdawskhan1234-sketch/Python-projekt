61f30f244453fd38ecc9aab8e6030434ec8408e6
Ferdaws khan <Ferdawaskhan1234@gmail.com>
2026-08-22 16:39:05 +0200

    Mina ändringar

--- programerin.py
+++ programerin.py
@@ -1,25 +1,79 @@
-my_lista = ["Faza", "Qesmat", "Moska", "Visal", "ferdaws", "khan"]
-print(my_lista)
-my_lista.append("khan11")
-print(my_lista)
+# my_lista = ["Faza", "Qesmat", "Moska", "Visal", "ferdaws", "khan"]
+# print(my_lista)
+# my_lista.append("khan11")
+# print(my_lista)
 
-you_lista = [2, 1, 3, 4, 6, 5, 7]
-you_lista.pop(3)
-print(you_lista)
+# you_lista = [2, 1, 3, 4, 6, 5, 7]
+# you_lista.pop(3)
+# print(you_lista)
 
-lista3 = [1, 2, 3, 4, 5,]
-lista3.insert(2, 100)
-print(lista3)
+# lista3 = [1, 2, 3, 4, 5,]
+# lista3.insert(2, 100)
+# print(lista3)
 
-lista4 = [2, 44, 5, 44, 33,33,33,33,33, 3, 3, 4, 4, 4]
-print(lista4.count(33))
+# lista4 = [2, 44, 5, 44, 33,33,33,33,33, 3, 3, 4, 4, 4]
+# print(lista4.count(33))
 
-lista5 = [2, 44, 5, 44, 33,33,33,33,33, 3, 3, 4, 4, 4]
-lista5.sort()
-print(lista5)
+# lista5 = [2, 44, 5, 44, 33,33,33,33,33, 3, 3, 4, 4, 4]
+# lista5.sort()
+# print(lista5)
 
-lista5.sort(reverse=True)
-print(lista5)
+# lista5.sort(reverse=True)
+# print(lista5)
 
-din_lista = ["Faza", "Qesmat", "Moska", "Visal", "ferdaws", "khan"]
-print(len(din_lista))
+# din_lista = ["Faza", "Qesmat", "Moska", "Visal", "ferdaws", "khan"]
+# print(len(din_lista))
+
+
+
+
+
+sok_list = [2, 3, 4, 5, 6,  4]
+sok_list.insert(3, 50)
+print(sok_list)
+
+den_lista = ["lima", "kashmier", "qesmat", "khan"]
+if "ferdaws" in den_lista:
+    print("nanm finn i lista")
+else:
+    print("namn finns inte i lista")
+
+
+
+är_lista = [22,3, 4, 5, 5, 7, 10]
+är_lista.remove(10)
+print(är_lista)
+
+
+
+l_lista = [3, 4, 2, 4, 3, 3 ,3 , 44, 4]
+print(l_lista.count(3))
+
+k_lista = [3, 4, 2, 4, 3, 3 ,3 , 44, 4]
+k_lista.sort(reverse=True)
+print(k_lista)
+
+
+a_lista = [3, 4, 2, 4, 3, 3 ,3 , 44, 4]
+a_lista.append(22)
+a_lista.insert(2, 11)
+a_lista.pop(-3)
+a_lista.sort()
+
+print(len(a_lista))
+print(a_lista)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
