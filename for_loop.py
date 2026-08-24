41e01269b7a613f90367885b704cfd089a9f7345
Ferdaws khan <Ferdawaskhan1234@gmail.com>
2026-08-25 01:32:54 +0200

    Min ändring

---
+++ for_loop.py
@@ -0,0 +1,196 @@
+# lista = [2,3,4,5,6,7]
+# for tal in lista:
+#     print(tal*2)
+
+
+# lista2 = ["Faza", "Qesmat", "Moska", "Visal", "ferdaws"]
+# for namn in lista2:
+#     print(namn)
+
+
+
+# lista3 = [2, 33, 4, 34, 10,55,2, 11, 2,4,]
+
+# for x in lista3:
+#     if x > 10:
+#         print(x)
+
+# lista4 = [2, 3, 4 ,4, 5, 6, 7, 8,66, 10, 44, ]
+# for z in lista4:
+#     if z > 10:
+#         print("stort")
+#     else:
+#         print("litet")
+  
+
+
+
+
+
+
+
+
+# lista5 = [2, 3, 4 ,4, 5, 6, 7, 8,66, 10, 44, ]
+
+# # count = 0
+# # for d in lista5:
+# #     if d > 10:
+# #         count += 1
+# #         print(count)
+
+
+
+
+
+# lista6 = [2, 3, 4 ,4, 5, 6, 7, 8,66, 10, 44, ]
+# count=0
+# for u in lista6:
+#     if u > 10:
+#         count+=1
+#         print(count)
+
+
+
+# lista7 = [3, 7, 12, 20, 5]
+# for n in lista7:
+#     if n ==12:
+#         print(n,  "finns") 
+
+
+
+
+
+# lista8 = [-3, 5, 7, -1, 8, 10, -7, 2]
+# count = 0
+# for j in lista8:
+#     if j >0:
+#        count+=1
+
+#        print(count)
+
+
+
+
+# lista9 = [3, 8, 2, 15, 6, 11, 10, 7]
+# for k in lista9:
+#     if k %2 ==0:
+
+
+#         print(k)
+        
+
+
+
+
+
+
+
+# lista11 = [-3, 5, 7, -1, 8, 10, -7, 2]
+# for f in lista11:
+#     if f <0:
+#         print(f)
+
+
+
+# lista22 =[11, 23, 34, 12, 8, 7, 5, 6,]
+# count=0
+# for t in lista22:
+
+#     if t >10:
+#         count+=1
+#         print(count)
+
+
+
+# lista33=[-3, 5, 7, -1, 8, 10, -7, 2]
+# for g in lista33:
+#     if g >0:
+#         print(g)
+
+
+
+
+# lista44=[-3, 5, 7, -9, -1, 8, 10, -7, 2]
+# for i in lista44:
+#     if i >0:
+       
+
+#           print(i, "Positivt")
+#     else:
+#          print( i,"Negativt eller 0")
+
+
+
+
+
+
+
+# lista55 = [3, 8, 11, 14, 7, 20, 5, 6]
+# for l in lista55:
+#     if l  % 2 ==1 :
+#         print(l, "Udda")
+#     else:
+#         print(l, "jämnt")
+
+
+
+# lista66 = [4, 12, 7, 25, 9, 18]
+# for h in lista66:
+#     if h  >18:
+#         print(h)   
+
+
+
+
+
+
+# lista77 = [4, 9, 12, 7, 20, 3, 16]
+# for d in lista77:
+#     if d % 2==0:
+#         print(d, "jämnt")
+#     else:
+#         print(d, "udda")
+
+
+
+
+
+
+# lista88 = [-3, 5, 8, -1, 10, -7, 2]
+# count=0
+# for s in lista88:
+#     if s >0:
+#         count+=1
+#         print( count)
+
+
+lista0 = [3, 8, 11, 14, 7, 20, 5]
+for a in lista0:
+    if a % 2==0:
+        print(a, "jämn")
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+   
+
+
+
+        
+        
+    
+
+
+       
+       
+
