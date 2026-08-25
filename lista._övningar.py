76b6145e321a17539c132be55fa3d67040a971c5
Ferdaws khan <Ferdawaskhan1234@gmail.com>
2026-08-26 01:19:20 +0200

    mina ändringar

--- lista._övningar.py
+++ lista._övningar.py
@@ -1,24 +1,55 @@
-lista2 = [22, 1, 4, 66, 2, 2, 2, 77, 10, 5, 12]
-print(lista2)
-lista2.remove(4)
-print(lista2)
-lista2.append(30)
-print(lista2)
-lista2.insert(1, 7)
-print(lista2)
-lista2.sort()
-print(lista2)
-print(lista2.count(2))
-print(lista2.count(5))
-print(len(lista2))
-lista2.remove(66)
-print(lista2)
-lista2.sort()
-print(lista2)
-lista2.reverse()
-print(lista2)
-lista3 =["banan", "äpple", "apelsin", "päron"]
-print(lista3)
-print(len(lista3))
+# lista2 = [22, 1, 4, 66, 2, 2, 2, 77, 10, 5, 12]
+# print(lista2)
+# lista2.remove(4)
+# print(lista2)
+# lista2.append(30)
+# print(lista2)
+# lista2.insert(1, 7)
+# print(lista2)
+# lista2.sort()
+# print(lista2)
+# print(lista2.count(2))
+# print(lista2.count(5))
+# print(len(lista2))
+# lista2.remove(66)
+# print(lista2)
+# lista2.sort()
+# print(lista2)
+# lista2.reverse()
+# print(lista2)
+# lista3 =["banan", "äpple", "apelsin", "päron"]
+# print(lista3)
+# print(len(lista3))
+
+
+
+
+
+# for i in range(1, 11):
+#     print(i)
+
+# for k in range(2, 22, 2, ):
+#     print(k)
+# for l in range(11, 0, -1):
+#     print(l)
+
+
+# for a in range(0, 21):
+#     if a >=10:
+#         print(a)
+# for b in range(0,21):
+#     if b % 2==1:
+#         print(b)
+# for d in range(0, 21):
+#     if d % 3==0:
+#         print(d)
+
+
+for s in range(1,31):
+    if s %5==0:
+        print(s)
+
+
+
 
 
