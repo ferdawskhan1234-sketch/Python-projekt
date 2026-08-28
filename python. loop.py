29370ec51769929380dc3eed106c0ef0efe48ea9
Ferdaws khan <Ferdawaskhan1234@gmail.com>
2026-08-28 02:25:41 +0200

    mina ändinrgar

--- python. loop.py
+++ python. loop.py
@@ -1,19 +1,63 @@
-list=[1, 2, 3, 44, 4, 2, 5, 6,]
-print(list)
+# list=[1, 2, 3, 44, 4, 2, 5, 6,]
+# print(list)
 
-list.pop(2)
-print(list)
-list.append(10)
-print(list)
-list.insert(1, 99)
-print(list)
-print(list.count(2))
-list.insert(3, 3)
-print(list)
-list.append(10)
-print(list)
-list.pop(3)
-print(list)
-list.insert(3, 99)
-print(list)
+# list.pop(2)
+# print(list)
+# list.append(10)
+# print(list)
+# list.insert(1, 99)
+# print(list)
+# print(list.count(2))
+# list.insert(3, 3)
+# print(list)
+# list.append(10)
+# print(list)
+# list.pop(3)
+# print(list)
+# list.insert(3, 99)
+# print(list)
 
+
+
+
+
+
+
+
+# a = 11
+# if a > 10:
+#     print("större än 10")
+# else:
+#     print("10 eller mindre")
+
+
+
+
+
+# h = 19
+# if h >= 18:
+     
+#      print("myndig")
+# else:
+
+#      print("Inte myndig")
+
+
+# y = 12
+# if y  >0:
+#     print("Positivt")
+# else:
+#     print("Negativt eller 0")
+
+
+# t =12
+# if t < 0:
+#     print("Negativt")
+# else:
+#     print("Positivt eller 0")
+    
+s = 10
+if s ==10:
+    print("Talet är 10")
+else:
+    print("Talet är inte 10")
