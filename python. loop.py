266db0606dc2ac1b53ea17d471c23b3b649d209e
Ferdaws khan <Ferdawaskhan1234@gmail.com>
2026-08-28 18:41:10 +0200

    mina ändringar

--- python. loop.py
+++ python. loop.py
@@ -56,8 +56,91 @@
 # else:
 #     print("Positivt eller 0")
     
-s = 10
-if s ==10:
-    print("Talet är 10")
+# s = 10
+# if s ==10:
+#     print("Talet är 10")
+# else:
+#     print("Talet är inte 10")
+
+
+
+# z = 21
+# if z > 20:
+#     print("större än 20")
+# elif z ==20:
+#     print("exakt 20")
+# else:
+#     print("mindre än 20")
+
+# ålder = 15
+# if ålder < 13:
+#     print("barn")
+# elif ålder <=17:
+#     print("tinåring")
+# else:
+#     print("vuxen")
+
+
+# t = 23
+# if t < 0:
+#     print("Minusgrader")
+# elif t >=0 and t <= 20:
+#     print("Kallt")
+# elif t >=21 and t <= 30:
+#     print("Behagligt")
+# else:
+#     print("värm")
+
+# n= 89
+# if n <=30:
+#     print("lågt")
+# elif n >=31 and n <=70:
+
+#     print("medel")
+# else:
+#     print("högt")
+
+
+# p = 55
+# if p >=90 and p <=100:
+#     print("A")
+# elif p >=80 and p <= 89:
+#     print("B")
+# elif p >=70 and p<=79:
+#     print("C")
+# elif p >=60 and p <=69:
+#     print("D")
+# else:
+#     print("F")
+
+# pengar= 1500
+# if pengar > 0 and pengar <=100:
+#     print("Billigt")
+# elif pengar >=101 and pengar <= 500:
+#     print("normalt")
+# elif pengar >=501 and pengar <=1000:
+#     print("Dyrt")
+# else:
+#     print(" Mycket dyrt")
+
+
+# m = 9
+# if not m ==5:
+#     print("inte 5")
+
+# else:
+#     print("tala är 5")
+
+
+# lista =["ferdaws", "khan", "visal", "Qesmat", "Ali", "Moska"]
+# if "Ali" in lista:
+#     print("finns i lista")
+# else:
+#     print("finns inte")
+
+
+lista1 =["ferdaws",  "khan", "visal", "Qesmat", "Ali", "Moska"]
+if "Sara" not in lista1:
+    print("finns inte  lista1")
 else:
-    print("Talet är inte 10")
+    print("finns  i lista1")
