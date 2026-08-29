b56f44028b5cc0748b613f21167fc7cf046b0fd5
Ferdaws khan <Ferdawaskhan1234@gmail.com>
2026-08-29 23:42:52 +0200

    mina ändring

---
+++ else.py
@@ -0,0 +1,137 @@
+# f = 10
+# if f > 10 and f < 20:
+#     print("Mellan 10 och 20")
+# elif f ==10 or f ==20:
+#     print("Exakt 10 eller 20")
+# else:
+#     print("Utanför intervallet")
+
+# lista =["ferdaws",  "khan", "visal","Omar", "Qesmat", "Ali", "Moska"]
+# if "Omar" not in lista:
+#     print("Omar finns inte")
+# else:
+#     print("Omar finns")
+
+
+# x = 55
+# y = 34
+# if x > 50 or y >50:
+#     print("godkänt")
+# else:
+#     print("inte godkänt")
+
+
+
+# a = 54
+# b = 55
+# if a >50 and b > 50:
+#     print("godkänt")
+# else:
+#     print("inte godkänt")
+
+# r = 55
+# e = 66
+# q = 100
+
+# if r > 50 and e > 50 or q ==100:
+#     print("gödkänt")
+# else:
+#     print("inte gödkänt")
+
+
+# h = 1
+# g = 8
+# if h > 10 and  g != 5:
+
+#     print("gödkänt")
+# else:
+#     print("inte gödkänt")
+
+
+# n = 10
+# if n !=10:
+#     print("inte 10")
+# else:
+#     print("Det är 10")
+
+# frukter = ["äpple",  "banan", "apelsin", "päron"]
+
+# if "banan" in frukter:
+#     print("banan finns")
+# else:
+#     print("banan finns inte")
+
+
+# f = 2
+# while f <= 5:
+#     print(f)
+#     f +=1
+
+# x = 1
+# while x <=10:
+#     print(x)
+#     x +=1
+
+
+# h = 1
+# while h <= 10:
+#     if h ==5:
+#         h +=1
+
+#         continue
+#     print(h)
+#     h +=1
+
+
+
+# f = 2
+# while f <=20:
+#     if f %2 ==0:
+#         print(f)
+
+#     f +=1'
+
+
+# e = 1
+# while e <= 15:
+#     if e % 2 ==1:
+#         print(e)
+#     e +=1
+
+# r =1
+# while r <=10:
+#     if r % 3 ==0:
+#         r+=1
+#         continue
+#     print(r)
+#     r +=1
+
+
+# w =1 
+# while w <=10:
+#     if w % 2 ==0:
+#         print(w, "jämnt")
+#         w +=1
+#     else:
+#         print(w, "udda")
+#         w+=1
+
+
+# d = 1
+# count =0
+
+# while d <=10:
+#     if d % 2==0:
+#         count+=1
+#     d+=1
+
+# print(count)
+     
+b =1
+count=0
+while b <= 20:
+    if b % 2==1:
+        count+=1
+    b+=1
+print(count)
+
