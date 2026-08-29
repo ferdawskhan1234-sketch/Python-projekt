b03d6a6989d0d039a5770a0ee7cd3f54a8c7d35e
Ferdaws khan <Ferdawaskhan1234@gmail.com>
2026-08-30 00:09:22 +0200

    mina ändringar

---
+++ while.py
@@ -0,0 +1,20 @@
+# x =10
+# while x <= 20:
+#     if x < 10:
+#         print("Litet")
+#     elif x ==10 :
+#         print("Exakt")
+#     else:
+#         print("Stort")
+
+
+
+
+
+
+tal = input("Skriv ett tal: ")
+
+while tal != "0":
+    tal = input("Skriv ett tal: ")
+
+print("Programmet avslutas")
