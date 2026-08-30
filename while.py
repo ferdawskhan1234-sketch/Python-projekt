3518aa37fa8c14928367c5b8bb68798835042845
Ferdaws khan <Ferdawaskhan1234@gmail.com>
2026-08-31 01:11:53 +0200

    mina ändringar

--- while.py
+++ while.py
@@ -13,9 +13,97 @@
 #     print(f)
 #     f+=1
 
-d= 1
-while d <=10 and d % 2==0:
-       print(d)
+# d= 1
+# while d <=10:
+#     if d % 2==0:
+#         print(d)
+#     d+=1
 
-       d+=1
 
+# j = 1
+# while j <=10:
+#     if j % 2==1:
+#         print(j)
+#     j+=1
+
+
+# c = 1
+# while c <=10:
+#     if c == 5:
+#         c+=1
+#         continue
+#     print(c)
+#     c+=1
+
+
+# l=1
+# while l <=20:
+#     if l ==12:
+#         break
+#     print(l)
+#     l +=1
+
+# x = 1
+# count = 0
+
+# while x <=20:
+#     if x % 2==0:
+#         count+=1
+#     x+=1
+# print(count)
+    
+
+# b = 1
+# count =0
+# while b <=10:
+#     if b > 5:
+#         count+=1
+#     b +=1
+
+# print(count)
+
+# k =1
+# count =0
+# while k <=20:
+#     if k > 10 and k % 2==0:
+#         count+=1
+#     k +=1
+# print(count)
+
+# a =1
+# count =0
+
+# while a <=30:
+#     if a % 2!=0 and a % 3 ==0:
+#         count+=1
+#     a+=1
+# print(count)
+
+
+
+
+# o =1
+# count = 0
+# while o <=30:
+#     if o % 3==0 or o % 5 ==0:
+#         count+=1
+#     o+=1
+# print(count)
+
+
+# u = 1
+# while u <=20:
+#     if u %4==0:
+#         u+=1
+#         continue
+#     print(u)
+#     u+=1
+
+y= 1
+count=0
+while y <=50:
+    if y % 2==0 and y >20 and y <40:
+        count+=1
+    y+=1
+print(count)
+        
