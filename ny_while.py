8d4369c22a0af21a334cf0dc9cd060fc4a80ef1f
Ferdaws khan <Ferdawaskhan1234@gmail.com>
2026-09-01 13:34:34 +0200

    mina ändringar

--- ny_while.py
+++ ny_while.py
@@ -23,14 +23,60 @@
 #         print("tio")
 #         y+=1
 
-d =1
-while d <=20:
-    if d %3==0:
-        print("Delbart med 3")
-        d+=1
-    elif d % 2==0:
-        print("delbart med 2")
-        d+=1
-    else:
-        print("inte delbart med 2 eller 3")
-        d+=1
+# d =1
+# while d <=20:
+#     if d %3==0:
+#         print("Delbart med 3")
+#         d+=1
+#     elif d % 2==0:
+#         print("delbart med 2")
+#         d+=1
+#     else:
+#         print("inte delbart med 2 eller 3")
+#         d+=1
+# f =1
+# while f <=30:
+#     if f % 5==0:
+#         print("fem")
+#         f+=1
+#     elif f% 3==0:
+#         print("tre")
+#         f+=1
+#     else:
+#         print("annat")
+#         f+=1
+
+# s =1
+# count=0
+# while s <= 30:
+#     if s % 3==0:
+#         count+=1
+#         print(count,)
+#         s+=1
+        
+#     elif s % 5 ==0:
+#         count+=1
+#     s+=1
+# print(count, )
+       
+
+# a =1
+# count=0
+# while a <= 30:
+#     if a % 2==0 and a >10:
+#         count+=1
+#     a+=1
+# print(count)
+
+q =1 
+count = 0
+while q <= 30:
+    if q % 2==0:
+        count+=1
+        
+    elif q % 3==0:
+        count+=1
+
+    q+=1
+print(count)
+
