b834e1206a59752d04110d056968dd1c68c7d797
Ferdaws khan <Ferdawaskhan1234@gmail.com>
2026-09-01 02:02:20 +0200

    min ändringar

---
+++ ny_while.py
@@ -0,0 +1,36 @@
+# x = 1
+# while x <=20:
+#     if x <5:
+#         print("Litet")
+#         x+=1
+#     elif x <=15:
+#         print("Mellan")
+#         x+=1
+#     else:
+#         print("sort")
+#         x+=1
+
+# y =1
+# while y <=20:
+#     if y %2==0:
+#         print("jämt")
+#         y+=1
+#     elif y % 2==1:
+#         print("udda")
+#         y+=1
+
+#     else:
+#         print("tio")
+#         y+=1
+
+d =1
+while d <=20:
+    if d %3==0:
+        print("Delbart med 3")
+        d+=1
+    elif d % 2==0:
+        print("delbart med 2")
+        d+=1
+    else:
+        print("inte delbart med 2 eller 3")
+        d+=1
