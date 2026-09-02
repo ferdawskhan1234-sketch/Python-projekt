f8681f99255b40a1fc2bc794c3a3e8a798dd9452
Ferdaws khan <Ferdawaskhan1234@gmail.com>
2026-09-03 01:30:55 +0200

    mina ändringar

--- 2_while.py
+++ 2_while.py
@@ -20,28 +20,136 @@
 #         print("Udda")
 #     q+=1
 
-w =1
-while w <=30:
-    if w %4==0:
-        print("fyra")
-    elif w % 2 ==0:
-         print("två")
+# w =1
+# while w <=30:
+#     if w %4==0:
+#         print("fyra")
+#     elif w % 2 ==0:
+#          print("två")
 
-    else:
-        print("annat")
-    w+=1
+#     else:
+#         print("annat")
+#     w+=1
 
 
 
-    e= 1
-    while e <=50:
-        if e ==15:
-            print("FEmton")
-        elif e % 5==0:
+    # e= 1
+    # while e <=50:
+    #     if e ==15:
+    #         print("FEmton")
+    #     elif e % 5==0:
             
-            print("fem")
-        elif e %2==0:
-            print("jämnt")
-        else:
-            print("annat")
-        e+=1
+    #         print("fem")
+    #     elif e %2==0:
+    #         print("jämnt")
+    #     else:
+    #         print("annat")
+    #     e+=1
+
+# r = 1
+# while r <= 50:
+#     if r == 20:
+#         print("tjugo")
+#     elif r % 4==0 and r % 5 ==0:
+#         print("Fyra och fem")
+#     elif r %4==0:
+#         print("fyra")
+#     else:
+#         print("Annat")
+#     r+=1
+# t = 1
+# while t <= 50:
+#     if t ==30:
+#         print("Tretti")
+#     elif t % 3 == 0 and t % 5==0:
+#         print("tre ocg fem")
+#     elif t % 3 ==0:
+#         print("tre")
+#     elif t % 2 ==0:
+#         print("jämnt")
+#     else:
+#         print("Annat")
+#     t+=1
+
+
+
+
+# y = 1
+# while y <=50:
+#     if y == 25:
+#         print("Tjugofem")
+#     elif y % 5==0 and y % 2==0:
+#         print("fem och jämnt")
+#     elif y % 5 ==0:
+#         print("fem")
+#     elif y % 2==1:
+#         print("Udda")
+#     else:
+#         print("Annat")
+#     y+=1
+
+u =1
+# while u <=50:
+#     if u == 40:
+#         print("Fyrtio")
+#     elif u % 4==0 and u % 2==0:
+#         print("fyra och jämnt")
+#     elif u % 4==0:
+#         print("fyra")
+#     elif u % 2==0 and u > 20:
+#         print("jämnt över 20")
+#     else:
+#         print("annat")
+#     u+=1
+
+
+
+
+# i = 1
+# while i <= 50:
+#     if i ==35:
+#         print("Trettiofem")
+#     elif i % 7==0 and i % 2==0:
+#         print("sju och jämnt")
+#     elif i % 7==0:
+#         print("sju")
+#     elif i % 2==1 and i > 25:
+#         print("udda över 25")
+#     else:
+#         print("Annat")
+#     i+=1
+
+
+# for  o  in range(10):
+#  print(o)
+
+# for p in range(2, 21):
+#     print(p)
+
+
+# for a in range(1, 20, 2):
+#     print(a)
+
+# for s in range(10, 0, -1):
+#     print(s)
+
+
+# for d in range(10, 0, -2):
+#     print(d)
+
+
+
+
+for f in range(1, 21):
+    if f % 3==0:
+        print(f)
+
+
+
+
+
+
+
+
+
+
