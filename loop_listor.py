c19b16a26e81151e24f1a117cfde372efc213073
Ferdaws khan <Ferdawaskhan1234@gmail.com>
2026-09-05 21:51:06 +0200

    mina ändringar

---
+++ loop_listor.py
@@ -0,0 +1,76 @@
+# count=0
+# namn = ["Ali", "Sara", "Omar", "Alexander", "Lina"]
+# for q in namn:
+#     if len(q) <=4:
+#         count+=1
+# print(count)
+
+
+
+
+
+
+
+# namn1 = ["Ali", "Sara", "Omar", "Alexander", "Lina", "Mohammed"]
+# n=[]
+# for w in namn1:
+#     if len(w)>4:
+#         n.append(w)
+# print(n)
+
+# namn2 = ["Ali", "Sara", "Omar", "Lina", "Adam"]
+# for e in namn2:
+#     if len(e) ==4:
+#      print(e)
+
+
+
+# namn4 = ["Ali", "Sara", "Omar", "Lina", "Alexander"]
+# print(namn4)
+# nam=[]
+# for t in namn4:
+#     if len(t) <=4:
+#         nam.append(t)
+# print(nam)
+
+
+
+
+# namn0 = ["Ali", "Sara", "Omar", "Lina", "Alexander", "Mohammed"]
+# m=[]
+# for k in namn0:
+#     if len(k)>4:
+#         m.append(k)
+# print(m)
+
+
+
+
+# namn9= ["Ali", "Sara", "Omar", "Lina", "Alexander", "Mohammed", "Eva"]
+# lista=[]
+# for b in namn9:
+#     if len(b)==3:
+#         lista.append(b)
+# print(lista)
+
+
+
+
+
+
+
+# namn8 = ["Ali", "Sara", "Omar", "Lina", "Alexander", "Eva", "Adam"]
+# lista1=[]
+# for c in namn8:
+#     if len(c) >= 3 and len(c)  < 5:
+#         lista1.append(c)
+# print(lista1)
+
+
+
+tal12 = [4, 7, 12, 15, 18, 21, 24, 30, 33]
+li=[]
+for s in tal12:
+    if s >10 and s <30 and s % 2==1:
+        li.append(s)
+print(li)
