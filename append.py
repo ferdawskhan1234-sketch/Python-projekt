4aebfc1ab629ef9b6efbcd3accaafda00329ccb5
Ferdaws khan <Ferdawaskhan1234@gmail.com>
2026-09-11 00:46:46 +0200

    mina ändringar

---
+++ append.py
@@ -0,0 +1,367 @@
+# count=0
+# summa=0
+# tal = [3, 8, 11, 14, 17, 20, 23, 26, 29, 32]
+# Udda=[]
+
+# for q in tal:
+#     if q % 2==1 and q > 10:
+#         Udda.append(q)
+#         count+=1
+#         summa+=q
+# print(Udda)
+# print(count)
+# print(summa)
+
+
+
+# count=0
+# summa=0
+# medel=0
+# tal1 = [6, 9, 12, 15, 18, 21, 24]
+# lista=[]
+# for w in tal1:
+#     if w > 10:
+#         lista.append(w)
+#         count+=1
+#         summa+=w
+#         medel= summa/count
+# print(lista)
+# print(count)
+# print(summa)
+# print(medel)
+
+
+
+# count=0
+# summa=0
+# mvärde=0
+# tal = [5, 8, 11, 14, 17, 20, 23, 26, 29, 32]
+# lista1=[]
+# for e in tal:
+#     if e % 2==1 and e >10:
+#         lista1.append(e)
+#         count+=1
+#         summa+=e
+#         mvärde = summa/count
+# print(lista1)
+# print(count)
+# print(summa)
+# print(mvärde)
+
+
+
+
+# count=0
+# summa=0
+# tal3 = [4, 7, 10, 13, 16, 19, 22, 25, 28]
+# jämna=[]
+# udda=[]
+# for r in tal3:
+#     if r % 2==0:
+#         jämna.append(r)
+#         count+=1
+#     elif r % 2==1:
+#         udda.append(r)
+#         summa+=r
+        
+# print(jämna)
+# print(count)
+# print(udda)
+# print(summa)
+
+
+
+# count=0
+# summa=0
+# tal4 = [5, 8, 11, 14, 17, 20, 23, 26, 29]
+# lis2ta=[]
+# for t in tal4:
+#     if t > 10 and t % 2==1:
+#         lis2ta.append(t)
+#         count+=1
+#         summa+=t
+# print(lis2ta)
+# print(count)
+# print(summa)
+
+
+
+
+
+# count=0
+# summa=0
+# medel=0
+# tal5 = [6, 9, 12, 15, 18, 21, 24, 27]
+# l1ista=[]
+# for y in tal5:
+#     if y >10 and y %2==0:
+#         l1ista.append(y)
+#         count+=1
+#         summa+=y
+#         medel=summa/count
+# print(l1ista)
+# print(count)
+# print(summa)
+# print(medel)
+
+
+
+
+
+
+# count_jämna = 0
+# count_udda = 0
+# tal6 = [4, 7, 12, 15, 18, 21, 24, 27, 30, 33]
+# li11ta=[]
+# li22ta=[]
+# for u in tal6:
+#      if u %2==0 and u >10:
+#          li11ta.append(u)
+#          count_jämna+=1
+#      elif u % 2 ==1 and u > 10:
+#          li22ta.append(u)
+#          count_udda+=1
+# print(li11ta)
+# print(count_jämna)
+# print(li22ta)
+# print(count_udda)
+
+
+# count_j=0
+# count_u=0
+# summa_j=0
+# summa_u=0
+# tal7 = [5, 8, 11, 14, 17, 20, 23, 26, 29, 32]
+# jämna1=[]
+# udda1=[]
+# for i in tal7:
+#     if i % 2==0 and i > 10:
+#         jämna1.append(i)
+#         count_j+=1
+#         summa_j+=i
+#     elif i %2==1 and i >10:
+#         udda1.append(i)
+#         count_u+=1
+#         summa_u+=i
+# print(jämna1)
+# print(count_j)
+# print(summa_j)
+# print(udda1)
+# print(count_u)
+# print(summa_u)
+
+
+# count_j=0
+# count_u=0
+# summa_j=0
+# summa_u=0
+# med=0
+# tal8 = [6, 9, 12, 15, 18, 21, 24, 27, 30]
+# jäm=[]
+# ud=[]
+# for o in tal8:
+#     if o % 2==0 and o > 10:
+#         jäm.append(o)
+#         count_j+=1
+#         summa_j+=o
+#         med=summa_j/count_j
+#     elif o % 2==1 and o > 10:
+#         ud.append(o)
+#         count_u+=1
+#         summa_u+=o
+#         med=summa_u/count_u
+# print(jäm)
+# print(count_j)
+# print(summa_j)
+# print(med)
+
+# print(ud)
+# print(count_u)
+# print(summa_u)
+# print(med)
+
+# count=0
+# summma=0
+# tal9 = [5, 8, 11, 14, 17, 20, 23, 26, 29, 32]
+# li=[]
+# for  p in tal9:
+#     if p %2==0 and p > 10:
+#         li.append(p)
+#         count+=1
+#         summma+=p
+#         print
+
+# print(li)
+# print(count)
+# print(summma)
+
+
+
+
+# t= [3, 7, 12, 15, 18, 22, 25, 30]
+# ta=[]
+# for a in t:
+#     if a > 10 or a % 5==0:
+#         ta.append(a)
+# print(ta)
+
+# l = [3, 8, 11, 14, 17, 20, 23, 26, 29]
+# a=[]
+# for s in l:
+#     if not s %2==0:
+#         a.append(s)
+# print(a)
+
+
+
+
+
+# ta1= [4, 7, 10, 13, 16, 19, 22, 25, 28, 31]
+# delbar=[]
+# for d in ta1:
+#     if not d %3==0:
+#         delbar.append(d)
+# print(delbar)
+
+# tl = [3, 6, 8, 10, 12, 15, 17, 20, 21, 25]
+# injm=[]
+# for f in tl:
+#     if not f %2==0 and f >10:
+#         injm.append(f)
+# print(injm)
+
+
+# t = [4, 7, 10, 13, 16, 19, 22, 25, 28, 31]
+
+# jämna = []
+
+# for g in t:
+#     if g % 2 == 0 or g % 5 == 0:
+#         jämna.append(g)
+
+# print(jämna)
+
+
+
+# tall = [3, 6, 8, 11, 14, 15, 18, 21, 24, 25]
+# större=[]
+# for h in tall:
+#     if h >10 and not h % 3==0:
+#         större.append(h)
+# print(större)
+
+
+
+
+# namn = ["Ali", "Sara", "Omar", "Lina", "Alexander", "Eva"]
+# n=[]
+# for j in namn:
+#     if len(j)>=4:
+#         n.append(j)
+# print(n)
+
+
+
+
+# namn1 = ["Ali", "Sara", "Omar", "Lina", "Alexander", "Eva"]
+# exakt=[]
+# for k in namn1:
+#     if len(k)==3 or k[0]=="A":
+#         exakt.append(k)
+# print(exakt)
+
+
+
+
+
+
+# namn2 = ["Ali", "Sara", "Omar", "Lina", "Alexander", "Eva"]
+# fyra=[]
+# for l in namn2:
+#     if len(l)>=4 and l[-1]=="a":
+#         fyra.append(l)
+# print(fyra)
+
+
+
+
+namn3 = ["Ali", "Sara", "Omar", "Lina", "Alexander", "Eva"]
+a=[]
+for z in namn3:
+    if not len(z) ==3 or z[0]=="A":
+        a.append(z)
+print(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
